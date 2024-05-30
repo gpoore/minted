0a84afca9775ea56f57dfbaa3fe9c7dd7f2ef134
Geoffrey Poore <gpoore@gmail.com>
2024-05-29 22:35:29 -0500

    [python] v0.1.0b3: security review: upgraded fmtversion.py to v1.2.0, which removes an unused function containing exec(); now exec() never appears (even unused) outside the restricted subpackage

--- python/latexminted/fmtversion.py
+++ python/latexminted/fmtversion.py
@@ -1,78 +1,98 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2015-2018, Geoffrey M. Poore
+# Copyright (c) 2015-2024, Geoffrey M. Poore
 # All rights reserved.
 #
 # Licensed under the BSD 3-Clause License:
-# http://opensource.org/licenses/BSD-3-Clause
+# https://opensource.org/license/BSD-3-clause
 #
 
 
 '''
-=============================================================
-``fmtversion``:  Simple version variables for Python packages
-=============================================================
+# `fmtversion`:  Simple version variables for Python packages
 
-:Author: Geoffrey M. Poore
-:License: `BSD 3-Clause <http://opensource.org/licenses/BSD-3-Clause>`_
+Converts version information into a string `__version__` and a named tuple
+`__version_info__` suitable for Python packages.  The approach is inspired
+by PEP 440 and `sys.version_info`:
 
-Converts version information into a string ``__version__`` and a namedtuple
-``__version_info__`` suitable for Python packages.  The approach is inspired
-by PEP 440 and ``sys.version_info``:
+  * https://www.python.org/dev/peps/pep-0440
 
-* https://www.python.org/dev/peps/pep-0440
-* https://docs.python.org/3/library/sys.html
+  * https://docs.python.org/3/library/sys.html
 
 Versions of the form "major.minor.micro" are supported, with an optional,
 numbered dev/alpha/beta/candidate/final/post status.  The module does not
 support more complicated version numbers like "1.0b2.post345.dev456", since
-this does not fit into a namedtuple of the form used by ``sys.version_info``.
+this does not fit into a named tuple of the form used by `sys.version_info`.
 The code is written as a single module, so that it may be bundled into
 packages, rather than needing to be installed as a separate dependency.
 
-Typical usage in a package's ``version.py`` with a bundled ``fmtversion.py``::
+Typical usage in a package's `version.py` with a bundled `fmtversion.py`:
 
-    from .fmtversion import get_version_plus_info
-    __version__, __version_info__ = get_version_plus_info(1, 1, 0, 'final', 0)
+```
+from .fmtversion import get_version_plus_info
+__version__, __version_info__ = get_version_plus_info(1, 2, 0, 'final', 0)
+```
 
-Following ``sys.version_info``, ``get_version_plus_info()`` takes arguments
-for a five-component version number:  major, minor, micro, releaselevel, and
-serial.  The releaselevel may be any of dev, alpha, beta, candidate, final, or
+Following `sys.version_info`, `get_version_plus_info()` takes arguments
+for a five-component version number:  major, minor, micro, release level, and
+serial.  The release level may be any of dev, alpha, beta, candidate, final, or
 post, or common variations/abbreviations thereof.  All arguments but
-releaselevel must be convertable to integers.
+release level must be convertable to integers.
 
-If only ``__version__`` or ``__version_info__`` is desired, then the functions
-``get_version()`` or ``get_version_info()`` may be used instead.  If a micro
-version is not needed (``<major>.<minor>.<micro>``), then set the optional
-keyword argument ``usemicro=False``.  This will omit a micro version from the
-string ``__version__``, while the namedtuple ``__version_info__`` will still
-have a field ``micro`` that is set to zero to simplify comparisons.  If each
-releaselevel will only have one release, then set ``useserial=False``.  This
-will omit a serial number from the string ``__version__``, while the
-namedtuple ``__version_info__`` will still have a field ``serial`` that is set
+If only `__version__` or `__version_info__` is desired, then the functions
+`get_version()` or `get_version_info()` may be used instead.  If a micro
+version is not needed (`<major>.<minor>.<micro>`), then set the optional
+keyword argument `usemicro=False`.  This will omit a micro version from the
+string `__version__`, while the named tuple `__version_info__` will still
+have a field `micro` that is set to zero to simplify comparisons.  If each
+release level will only have one release, then set `useserial=False`.  This
+will omit a serial number from the string `__version__`, while the
+named tuple `__version_info__` will still have a field `serial` that is set
 to zero.
 
-A function ``get_version_from_version_py_str()`` is included for extracting
-the version from a ``version.py`` file that has been read into a string.  This
-is intended for use in ``setup.py`` files.
+
+## License
+
+BSD 3-Clause License
+
+Copyright (c) 2015-2024, Geoffrey M. Poore
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
 
 
 
 
-from __future__ import (division, print_function, absolute_import,
-                        unicode_literals)
-import sys
-if sys.version_info.major == 2:
-    str = basestring
 import collections
 
 
 # Version of this module, which will later be converted into a `__version__`
 # and `__version_info__` once the necessary functions are created
-__version_tuple__ = (1, 1, 0, 'final', 0)
-
-__docformat__ = 'restructuredtext en'
+__version_tuple__ = (1, 2, 0, 'final', 0)
 
 
 
@@ -156,7 +176,7 @@ def get_version(*args, **kwargs):
         version += '.{0}'.format(version_info.micro)
     if version_info.releaselevel != 'final':
         if version_info.releaselevel in ('dev', 'post'):
-                version += '.{0}'.format(version_info.releaselevel)
+            version += '.{0}'.format(version_info.releaselevel)
         else:
             version += '{0}'.format(version_info.releaselevel)
         if useserial:
@@ -184,34 +204,3 @@ def get_version_plus_info(*args, **kwargs):
 # Now that the required functions exist, process `__version_tuple__` into
 # `__version__` and `__version_info__` for the module
 __version__, __version_info__ = get_version_plus_info(*__version_tuple__)
-
-
-
-
-
-def get_version_from_version_py_str(string):
-    '''
-    Extract version from version.py that has been read into a string.
-
-    This assumes a simple, straightforward version.py.  It is meant for use
-    in setup.py files.
-    '''
-    if not isinstance(string, str):
-        raise TypeError('Argument must be a string')
-    if string.count('__version__') != 1:
-        raise RuntimeError('Failed to extract version from string')
-    exec_locals = {}
-    for line in string.splitlines():
-        if line.startswith('__version__'):
-            if not any(x in line for x in ['get_version(', 'get_version_plus_info(']):
-                raise RuntimeError('Failed to extract version from string')
-            if 'fmtversion.get_version' in line:
-                line = line.replace('fmtversion.get_version', 'get_version')
-            try:
-                exec(compile(line, 'version.py', 'exec'), globals(), exec_locals)
-            except Exception:
-                raise RuntimeError('Failed to extract version from string')
-            break
-    if '__version__' not in exec_locals:
-        raise RuntimeError('Failed to extract version from string')
-    return exec_locals['__version__']
