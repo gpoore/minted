74a41fc973412064dbe750e2f5e976b7edf3427d
Geoffrey Poore <gpoore@gmail.com>
2026-03-03 18:40:07 -0600

    fixed typos (closes #477)

--- python/latexminted/fmtversion.py
+++ python/latexminted/fmtversion.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2015-2024, Geoffrey M. Poore
+# Copyright (c) 2015-2026, Geoffrey M. Poore
 # All rights reserved.
 #
 # Licensed under the BSD 3-Clause License:
@@ -37,7 +37,7 @@ Following `sys.version_info`, `get_version_plus_info()` takes arguments
 for a five-component version number:  major, minor, micro, release level, and
 serial.  The release level may be any of dev, alpha, beta, candidate, final, or
 post, or common variations/abbreviations thereof.  All arguments but
-release level must be convertable to integers.
+release level must be convertible to integers.
 
 If only `__version__` or `__version_info__` is desired, then the functions
 `get_version()` or `get_version_info()` may be used instead.  If a micro
@@ -55,7 +55,7 @@ to zero.
 
 BSD 3-Clause License
 
-Copyright (c) 2015-2024, Geoffrey M. Poore
+Copyright (c) 2015-2026, Geoffrey M. Poore
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -92,7 +92,7 @@ import collections
 
 # Version of this module, which will later be converted into a `__version__`
 # and `__version_info__` once the necessary functions are created
-__version_tuple__ = (1, 2, 0, 'final', 0)
+__version_tuple__ = (1, 2, 1, 'final', 0)
 
 
 
@@ -124,7 +124,7 @@ def get_version_info(major, minor, micro, releaselevel, serial,
         micro = int(micro)
         serial = int(serial)
     except ValueError:
-        raise ValueError('major, minor, micro, and serial must be convertable to integers')
+        raise ValueError('major, minor, micro, and serial must be convertible to integers')
     if any(x < 0 for x in (major, minor, micro, serial)):
         raise ValueError('major, minor, micro, and serial must correspond to non-negative integers')
     if not usemicro and micro != 0:
